ccca4b22c6a45bb90e5aa0ab5fa45366c7ffc1a9
broken1arrow <broken2arrow1@gmail.com>
2021-05-13 13:49:55 +0200

    Add files via upload

---
+++ src/main/java/org/broken/repaircost/CalculateNumbers.java
@@ -0,0 +1,259 @@
+package org.broken.repaircost;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.Configurable;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class CalculateNumbers extends PlaceholderExpansion implements Configurable {
+	private final ConfigSettings config = new ConfigSettings(this);
+
+	boolean TwoDecimal = false;
+
+	public void CalculateNumber() {
+		CalculateNumbers instance = this;
+		config.reload();
+
+	}
+
+
+	@Override
+	public String onRequest(OfflinePlayer player, String identifier) {
+
+		if (identifier.startsWith("numbervalue")) {
+			try {
+				String[] StringInput;
+				int length = (StringInput = identifier.split("(?<!\\\\),")).length;
+				for (int i = 0; i < length; i++) {
+					String output = StringInput[i].replaceAll("\\\\,", ",");
+
+					if (output.startsWith("decimal"))
+						TwoDecimal = true;
+					if (output.startsWith("tool")) {
+						double outputNumber = listOfStringValues((Player) player, output);
+
+						ItemStack inHandCurentDurability = ((Player) player).getItemInHand();
+						short curentDurability = inHandCurentDurability.getDurability();
+						double numberOutput = (mathFormula(curentDurability, outputNumber));
+
+						if (TwoDecimal) {
+							TwoDecimal = false;
+							return String.valueOf(numberOutput);
+						} else return String.valueOf(Integer.valueOf((int) numberOutput));
+					}
+
+					if (output.startsWith("armor")) {
+						double outputNumber = listOfStringValues((Player) player, output);
+						double listOfArmorValue = listOfArmor((Player) player);
+
+						double outputarmorDmgNumber = (listOfArmorValue / outputNumber);
+						double roundOff = Math.round(outputarmorDmgNumber * 100.0) / 100.0;
+						if (TwoDecimal) {
+							TwoDecimal = false;
+							return String.valueOf(roundOff);
+						} else return String.valueOf(Integer.valueOf((int) outputarmorDmgNumber));
+					}
+				}
+			} catch (Throwable ex) {
+				ex.printStackTrace();
+			}
+		}
+		boolean emptyPlaceholder = config.getCustomConfig().getBoolean("EmptyPlaceholder");
+		if (!emptyPlaceholder)
+			return "[CalcuateNumbers] You has type invalid placeholder, if you think is a bug contact the author.";
+
+		return "";
+	}
+
+	private double mathFormula(short durability, double outputNumber) {
+		String stringDurability = String.valueOf(durability);
+		String stringoutputNumber = String.valueOf(outputNumber);
+		try {
+			String formulaString = config.getCustomConfig().getString("Math");
+			String total = formulaString.replace("{toolDurability}", stringDurability).replace("{valueNumber}", stringoutputNumber);
+			String[] splite = total.split(" ");
+
+
+			String secondInput = splite[1];
+			double firstInput = Double.parseDouble(splite[0]);
+			double threeInput = Double.parseDouble(splite[2]);
+
+			if (secondInput.equals("+") || secondInput.equals("-") || secondInput.equals("/") || secondInput.equals("*")) {
+				switch (secondInput) {
+					case ("+"):
+						return (firstInput + threeInput);
+					case ("-"):
+						return (firstInput - threeInput);
+					case ("/"):
+						return (firstInput / threeInput);
+
+					case ("*"):
+						return (firstInput * threeInput);
+				}
+
+			}
+		} catch (Throwable ex) {
+			ex.printStackTrace();
+		}
+		return 0;
+
+	}
+
+	private double listOfArmor(Player player) {
+		PlayerInventory inv = player.getInventory();
+		ItemStack helmet = inv.getHelmet();
+		ItemStack chest = inv.getChestplate();
+		ItemStack boots = inv.getBoots();
+		ItemStack leggings = inv.getLeggings();
+		double helm = 0.0;
+		double ChestPlate = 0.0;
+		double legging = 0.0;
+		double boot = 0.0;
+
+		ConfigurationSection congest = config.getCustomConfig();
+		ConfigurationSection configSection = congest.getConfigurationSection("Armor");
+		try {
+			if (configSection != null) {
+				for (String key : configSection.getKeys(true)) {
+
+					if (helmet != null) {
+						short helmetDurability = helmet.getDurability();
+						if (helmet.getType() == Material.LEATHER_HELMET && key.equals("Helmet.Leather"))
+							helm = (helmetDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (helmet.getType() == Material.LEGACY_GOLD_HELMET && key.equals("Helmet.Gold"))
+							helm = (helmetDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (helmet.getType() == Material.CHAINMAIL_HELMET && key.equals("Helmet.Chainmail"))
+							helm = (helmetDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (helmet.getType() == Material.IRON_HELMET && key.equals("Helmet.Iron"))
+							helm = (helmetDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (helmet.getType() == Material.DIAMOND_HELMET && key.equals("Helmet.Dimond"))
+							helm = (helmetDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (helmet.getType() == Material.NETHERITE_HELMET && key.equals("Helmet.Netherite"))
+							helm = (helmetDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+					}
+
+					if (chest != null) {
+						short chestDurability = chest.getDurability();
+						if (chest.getType() == Material.LEATHER_CHESTPLATE && key.equals("ChestPlate.Leather"))
+							ChestPlate = (chestDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (chest.getType() == Material.LEGACY_GOLD_CHESTPLATE && key.equals("ChestPlate.Gold"))
+							ChestPlate = (chestDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (chest.getType() == Material.CHAINMAIL_CHESTPLATE && key.equals("ChestPlate.Chainmail"))
+							ChestPlate = (chestDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (chest.getType() == Material.IRON_CHESTPLATE && key.equals("ChestPlate.Iron"))
+							ChestPlate = (chestDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (chest.getType() == Material.DIAMOND_CHESTPLATE && key.equals("ChestPlate.Dimond"))
+							ChestPlate = (chestDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (chest.getType() == Material.NETHERITE_CHESTPLATE && key.equals("ChestPlate.Netherite"))
+							ChestPlate = (chestDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+					}
+
+					if (leggings != null) {
+						short leggingsDurability = leggings.getDurability();
+						if (leggings.getType() == Material.LEATHER_LEGGINGS && key.equals("Leggings.Leather"))
+							legging = (leggingsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (leggings.getType() == Material.LEGACY_GOLD_LEGGINGS && key.equals("Leggings.Gold"))
+							legging = (leggingsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (leggings.getType() == Material.CHAINMAIL_LEGGINGS && key.equals("Leggings.Chainmail"))
+							legging = (leggingsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (leggings.getType() == Material.IRON_LEGGINGS && key.equals("Leggings.Iron"))
+							legging = (leggingsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (leggings.getType() == Material.DIAMOND_LEGGINGS && key.equals("Leggings.Dimond"))
+							legging = (leggingsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (leggings.getType() == Material.NETHERITE_LEGGINGS && key.equals("Leggings.Netherite"))
+							legging = (leggingsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+					}
+
+					if (boots != null) {
+						short bootsDurability = boots.getDurability();
+						if (boots.getType() == Material.LEATHER_BOOTS && key.equals("Boots.Leather"))
+							boot = (bootsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (boots.getType() == Material.LEGACY_GOLD_BOOTS && key.equals("Boots.Leather"))
+							boot = (bootsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (boots.getType() == Material.CHAINMAIL_BOOTS && key.equals("Boots.Leather"))
+							boot = (bootsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (boots.getType() == Material.IRON_BOOTS && key.equals("Boots.Leather"))
+							boot = (bootsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (boots.getType() == Material.DIAMOND_BOOTS && key.equals("Boots.Leather"))
+							boot = (bootsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+						else if (boots.getType() == Material.NETHERITE_BOOTS && key.equals("Boots.Leather"))
+							boot = (bootsDurability * Double.parseDouble(configSection.getString(key, String.valueOf(0.0))));
+					}
+				}
+			}
+		} catch (Throwable ex) {
+			System.out.println("[CalcuateNumbers] You has an error in your yml config file");
+			ex.printStackTrace();
+		}
+
+		return (helm + ChestPlate + legging + boot);
+	}
+
+	private double listOfStringValues(Player players, String input) {
+		String[] inputSplited;
+
+		int length = (inputSplited = input.split(":")).length;
+		for (int i = 0; i < length; i++) {
+			String inputs = inputSplited[i];
+
+			if (i == 1) {
+				String output = PlaceholderAPI.setBracketPlaceholders(players, inputs);
+
+				for (String list : listning()) {
+					String[] splited = list.split(": ");
+					String outputvalue = splited[0];
+
+					if (output.equals(outputvalue)) {
+						return Double.parseDouble(splited[1]);
+					}
+				}
+			}
+		}
+		return 1;
+	}
+
+	public List<String> listning() {
+		List<String> values = config.getCustomConfig().getStringList("Values");
+		System.out.println(values);
+		if (values.isEmpty())
+			return Collections.singletonList("Warn!!! Values missing or Values are emty");
+		return values;
+	}
+
+
+	@Override
+	public Map<String, Object> getDefaults() {
+		return null;
+	}
+
+	@Override
+	public @NotNull String getIdentifier() {
+		return "number";
+	}
+
+	@Override
+	public @NotNull String getAuthor() {
+		return "broken_arrow";
+	}
+
+	@Override
+	public boolean canRegister() {
+		return true;
+	}
+
+	@Override
+	public @NotNull String getVersion() {
+		return "0.60";
+	}
+
+}
---
+++ src/main/java/org/broken/repaircost/ConfigSettings.java
@@ -0,0 +1,139 @@
+package org.broken.repaircost;
+
+import me.clip.placeholderapi.PlaceholderAPIPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class ConfigSettings {
+
+	private final CalculateNumbers calculateNumbers;
+
+	private final PlaceholderAPIPlugin plugin;
+	private File customConfigFile;
+	private FileConfiguration customConfig;
+
+
+	public ConfigSettings(CalculateNumbers calculateNumbers) {
+		this.calculateNumbers = calculateNumbers;
+		plugin = calculateNumbers.getPlaceholderAPI();
+		reload();
+	}
+
+	public void reload() {
+		if (customConfigFile == null) {
+			customConfigFile = new File(plugin.getDataFolder(), "repaircostConfig.yml");
+
+			customConfig = YamlConfiguration.loadConfiguration(customConfigFile);
+
+			customConfig.options().header("Dynamic change numbers, config version " + calculateNumbers.getVersion()
+					+ "\nThis is main config for Calulate damage on tools and wapons"
+					+ "\n"
+					+ "\nYou put in your rank name and a number as some standard, it will"
+					+ "\ndivide your number with how much damage the tool has lost."
+					+ "\nYou can for now only change the order and use <+,/,*,->,"
+					+ "\nwill look on fix for this."
+					+ "\n"
+					+ "\nUse the placeholder like this, it need to output same name has you"
+					+ "\nset up in the config"
+					+ "\n%number_numbervalue,tool:{a placeholder from your ranking plugin}%"
+					+ "\n"
+					+ "\nIf you want to have two decimals use"
+					+ "\n%number_numbervalue,decimal,tool:{a placeholder from your ranking plugin}%"
+					+ "\n"
+					+ "\nFor calcuate armor values use placeholder below."
+					+ "\n%number_numbervalue,armor:{a placeholder from your ranking plugin}%"
+					+ "\n"
+					+ "\nIf you want to have two decimals use this placeholder"
+					+ "\n%number_numbervalue,decimal,armor:{a placeholder from your ranking plugin}%"
+					+ "\n"
+					+ "\nDo like this:"
+					+ "\nValues:"
+					+ "\n- 'Examplerank: 5'"
+					+ "\n");
+
+			if (customConfig.getKeys(false).isEmpty()) {
+				customConfig.set("EmptyPlaceholder", false);
+				customConfig.set("Values", new String[]{"Examplerank: 6", "Examplerank1: 4", "Examplerank2: 2"});
+				customConfig.set("Math", "{toolDurability} / {valueNumber}");
+				//      Helmet
+				customConfig.set("Armor.Helmet.Leather", "1.75");
+				customConfig.set("Armor.Helmet.Chainmail", "1.75");
+				customConfig.set("Armor.Helmet.Gold", "1.50");
+				customConfig.set("Armor.Helmet.Iron", "1.75");
+				customConfig.set("Armor.Helmet.Dimond", "2");
+				customConfig.set("Armor.Helmet.Netherite", "3");
+				//     ChestPlate
+				customConfig.set("Armor.ChestPlate.Leather", "1.75");
+				customConfig.set("Armor.ChestPlate.Chainmail", "1.75");
+				customConfig.set("Armor.ChestPlate.Gold", "1.5");
+				customConfig.set("Armor.ChestPlate.Iron", "1.75");
+				customConfig.set("Armor.ChestPlate.Dimond", "2");
+				customConfig.set("Armor.ChestPlate.Netherite", "3");
+				//     Leggings
+				customConfig.set("Armor.Leggings.Leather", "1.75");
+				customConfig.set("Armor.Leggings.Chainmail", "1.75");
+				customConfig.set("Armor.Leggings.Gold", "1.5");
+				customConfig.set("Armor.Leggings.Iron", "1.75");
+				customConfig.set("Armor.Leggings.Dimond", "2");
+				customConfig.set("Armor.Leggings.Netherite", "3");
+				//     Boots
+				customConfig.set("Armor.Boots.Leather", "1.75");
+				customConfig.set("Armor.Boots.Chainmail", "1.75");
+				customConfig.set("Armor.Boots.Gold", "1.5");
+				customConfig.set("Armor.Boots.Iron", "1.75");
+				customConfig.set("Armor.Boots.Dimond", "2");
+				customConfig.set("Armor.Boots.Netherite", "3");
+
+			}
+			save();
+		}
+	}
+
+
+	public FileConfiguration load() {
+		if (customConfig == null) reload();
+		return customConfig;
+	}
+
+	public void save() {
+		if (customConfig == null || customConfigFile == null) {
+			return;
+		}
+
+		try {
+			load().save(customConfigFile);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public String getContents(File customConfigFile) {
+		final StringBuilder sb = new StringBuilder();
+
+		try {
+			List<String> lines = Files.readAllLines(customConfigFile.toPath());
+			lines.forEach((line) -> sb.append(line).append("\n"));
+		} catch (IOException e) {
+			return null;
+		}
+
+		return sb.toString();
+	}
+
+	public File getCustomConfigFile() {
+		return customConfigFile;
+	}
+
+	public FileConfiguration getCustomConfig() {
+		return customConfig;
+	}
+
+}
+
+
+
